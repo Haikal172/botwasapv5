3000044e4662df1ffb72982470f291749c52d357
Haikal172 <80373583+Haikal172@users.noreply.github.com>
2021-03-14 07:50:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,21 +40,21 @@ const { ind } = require('./language')
 /********** MENU SETTING **********/
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
+            + 'FN:HAIKAL BOT\n' 
             + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6287875369113:+6287875369113\n' 
             + 'END:VCARD' 
 blocked = []   
 prefix = '#'
 limitawal = 30
 memberlimit = 0
 ator = 'SUBSCRIBE'
-namo = 'AMPIBI GAMING'
+namo = 'Leader 999'
 cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6287875369113@s.whatsapp.net","6287875369113@s.whatsapp.net"] 
 /************************************/
 
        
